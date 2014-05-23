fe97a3684f498bc10a9d92cc86bce7c2faf1444d
DoctorVanGogh <DoctorVanGogh@users.noreply.github.com>
2014-05-24 00:11:15 +0200

    added color config

--- Athena.lua
+++ Athena.lua
@@ -1,7 +1,6 @@
 -----------------------------------------------------------------------------------------------
 -- Client Lua Script for Athena 
--- Original CraftingGrid routines Copyright (c) Carbine, LLC and NCSOFT Corporation
--- Additional function & changes Copyright (c) DoctorVanGogh on Wildstar Forums. All rights reserved
+-- Copyright (c) DoctorVanGogh on Wildstar Forums. All rights reserved
 -- Wildstar ©2011-2014 Carbine, LLC and NCSOFT Corporation
 -----------------------------------------------------------------------------------------------
  
@@ -11,12 +10,23 @@ require "CraftingLib"
 -----------------------------------------------------------------------------------------------
 -- Athena Module Definition
 -----------------------------------------------------------------------------------------------
-local Athena = Apollo.GetPackage("Gemini:Addon-1.0").tPackage:NewAddon("Athena", false, { "CraftingGrid", }, "Gemini:Hook-1.0")
+local NAME = "Athena"
 
+local Athena = Apollo.GetPackage("Gemini:Addon-1.0").tPackage:NewAddon(
+																NAME, 
+																true, 
+																{ 
+																	"CraftingGrid", 
+																	"Gemini:Logging-1.2", 
+																	"GeminiColor",
+																	"Gemini:Locale-1.0"
+																}, 
+																"Gemini:Hook-1.0")
+															
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
-local ktHintArrowColors = {
+local ktHintArrowDefaultColors = {
 	[CraftingLib.CodeEnumCraftingDiscoveryHotCold.Cold] = ApolloColor.new("ConMinor"),
 	[CraftingLib.CodeEnumCraftingDiscoveryHotCold.Warm] = ApolloColor.new("ConModerate"),
 	[CraftingLib.CodeEnumCraftingDiscoveryHotCold.Hot] = ApolloColor.new("ConTough")
@@ -61,24 +71,74 @@ local mtSchematicLog = {
 	end
 }
 
+local glog
+local GeminiColor
+local GeminiLocale
+local GeminiLogging
 
 -----------------------------------------------------------------------------------------------
 -- Athena OnInitialize
 -----------------------------------------------------------------------------------------------
 function Athena:OnInitialize()
+	GeminiLogging = Apollo.GetPackage("Gemini:Logging-1.2").tPackage
+	glog = GeminiLogging:GetLogger({
+		level = GeminiLogging.DEBUG,
+		pattern = "%d [%c:%n] %l - %m",
+		appender = "GeminiConsole"
+	})	
 
+	self.log = glog
+	
+	GeminiColor= Apollo.GetPackage("GeminiColor").tPackage
+	self.gcolor = GeminiColor
+		
+	
+	GeminiLocale = Apollo.GetPackage("Gemini:Locale-1.0").tPackage		
+	self.localization = GeminiLocale:GetLocale(NAME)
+	
+	self.tColors = ktHintArrowDefaultColors
+	
 	self.IsCraftingGridHooked = self:Hook_CraftingGrid()
 	
 	-- preinitialize in case there is *no* data to deserialize in <see cref="Athena.OnRestore" /> and it never
 	-- get's called.
-	self.tLastMarkersList = {}
-	setmetatable(self.tLastMarkersList, mtSchematicLog)	
+	self.tLastMarkersList = setmetatable({}, mtSchematicLog)	
 		
+    -- load our form file
+	self.xmlDoc = XmlDoc.CreateFromFile("AthenaConfigForm.xml")
+	self.xmlDoc:RegisterCallback("OnDocumentReady", self)		
+	
 	if self.IsCraftingGridHooked then	
 		self:CheckForCraftingGridMarkerListInitialization()
 	end
 end
 
+
+function Athena:OnDocumentReady()
+	glog:debug(string.format("OnDocumentReady"))
+
+	if self.xmlDoc == nil then
+		return
+	end
+	
+	self.wndMain = Apollo.LoadForm(self.xmlDoc, "AthenaConfigForm", nil, self)
+	self.xmlDoc = nil;
+	
+	Apollo.RegisterSlashCommand("athena", "OnSlashCommand", self)
+	
+	self:InitializeForm()
+	
+	self.wndMain:Show(false);
+end
+
+function Athena:OnSlashCommand(strCommand, strParam)
+	self:ToggleWindow()
+end
+
+function Athena:OnConfigure(sCommand, sArgs)
+	self.wndMain:Show(false)
+	self:ToggleWindow()
+end
 
 -----------------------------------------------------------------------------------------------
 -- Athena Hooks for CraftingGrid
@@ -105,20 +165,77 @@ function Athena:RedrawAll()
 					--Print("WARN: No raw hot/cold data for schematic "..tCurrentCraft.nSchematicId.." attempt #"..idx)
 				else
 					--Print("INFO: schematic "..tCurrentCraft.nSchematicId.." attempt #"..idx.." = ".. eHotCold)
-					wndMarker:SetBGColor(ktHintArrowColors[eHotCold])
+					wndMarker:SetBGColor(self.tColors[eHotCold])
 				end
 			end		
 		end
 	end			
 end
 
-function Athena:OnDocumentReady()
+function Athena:CraftingGrid_OnDocumentReady()
 	Athena:CopyRestoredMarkersToCraftingGrid()
 end
 
 -----------------------------------------------------------------------------------------------
 -- Athena functions
 -----------------------------------------------------------------------------------------------
+function Athena:UpdateColorContainer(container, key)	
+	local color = self.tColors[key]
+	local colorHex
+	
+	glog:debug(string.format("UpdateColorContainer(%s) - color=%s", tostring(key), tostring(color)))
+		
+	if type(color) == "table" then
+		colorHex = self.gcolor:RGBAPercToHex(color.r, color.g, color.b, color.a)
+	elseif type(color) == "string"  then
+		colorHex = color
+	elseif type(color) == "userdata" then
+		color = color:ToTable()
+		colorHex = self.gcolor:RGBAPercToHex(color.r, color.g, color.b, color.a)		
+	end
+	
+	container:SetData(key)
+	
+	local picker = container:FindChild("ColorPickerButton")
+	
+	picker:UpdatePixie(1, {
+		strSprite = "BasicSprites:WhiteFill",
+		cr = color ,
+		loc = { fPoints = {0,0,1,1}, nOffsets = {4,4,-4,-3}}		
+	})
+	picker:SetData(colorHex)
+	local edit = container:FindChild("ColorValueEdit")
+	edit:SetText(colorHex:upper())
+
+	local preview = container:FindChild("ArrowPreview")
+	preview:SetBGColor(color)
+end
+
+
+function Athena:InitializeForm()
+	if not self.wndMain then
+		return
+	end
+	
+	GeminiLocale:TranslateWindow(self.localization, self.wndMain)		
+
+	self.wndMain:FindChild("HeaderLabel"):SetText(NAME)
+	local tColors = {
+		["ColorHot"] = CraftingLib.CodeEnumCraftingDiscoveryHotCold.Hot,
+		["ColorWarm"] = CraftingLib.CodeEnumCraftingDiscoveryHotCold.Warm,
+		["ColorCold"] = CraftingLib.CodeEnumCraftingDiscoveryHotCold.Cold	
+	}
+	
+	for strName, key in pairs(tColors) do
+		self:UpdateColorContainer(self.wndMain:FindChild(strName), key)
+	end
+	
+	if self.locSavedWindowLoc then
+		self.wndMain:MoveToLocation(self.locSavedWindowLoc)
+	end	
+end
+
+
 -- Define general functions here
 function Athena:Hook_CraftingGrid()
 	local tCraftingGrid = Apollo.GetAddon("CraftingGrid")
@@ -127,7 +244,7 @@ function Athena:Hook_CraftingGrid()
 	end
 	
 	self:PostHook(tCraftingGrid ,"RedrawAll")
-	self:PostHook(tCraftingGrid ,"OnDocumentReady")	
+	self:PostHook(tCraftingGrid ,"OnDocumentReady", "CraftingGrid_OnDocumentReady")	
 	
 	-- store reference to <see cref="CraftingGrid" />
 	self.tCraftingGrid = tCraftingGrid		
@@ -189,6 +306,22 @@ function Athena:StoreV1Data(tMarkers)
 			}		
 		end
 	end	
+	
+	
+	tSave.tColors = {}
+	for key,col in pairs(self.tColors) do	
+		if type(col) == "table" then
+			tSave.tColors[key] = self.gcolor:RGBAPercToHex(col.r, col.g, col.b, col.a)
+		elseif type(col) == "string"  then
+			tSave.tColors[key] = col
+		elseif type(col) == "userdata" then
+			local color = col:ToTable()
+			tSave.tColors[key] = self.gcolor:RGBAPercToHex(color.r, color.g, color.b, color.a)		
+		end	
+	end	
+	
+	local locWindowLocation = self.wndMain and self.wndMain:GetLocation() or self.locSavedWindowLoc						
+	tSave.tWindowLocation = locWindowLocation and locWindowLocation:ToTable() or nil
 
 	return tSave
 end
@@ -222,8 +355,8 @@ function Athena:RestoreV1Data(tData)
 			tEntries[entryKey] = tEntry 			
 		end
 	end		
-	
-	return tJournal 
+		
+	return tJournal, tData.tColors, tData.tWindowLocation 
 end	
 
 
@@ -249,7 +382,7 @@ function Athena:OnRestoreSettings(eLevel, tData)
 		return
 	end
 
-	local tLastMarkers = {}	
+	local tLastMarkers, tColors, tWindowLocation
 			
 	local version = tData.version	
 	
@@ -257,20 +390,74 @@ function Athena:OnRestoreSettings(eLevel, tData)
 		-- sry, can't read unversioned data
 	else
 		if version == "1" then
-			tLastMarkers = self:RestoreV1Data(tData)	
+			tLastMarkers, tColors, tWindowLocation = self:RestoreV1Data(tData)	
 		end		
-	end	
+	end		
+		
+	--[[
+		 Do *NOT* deserialize into <see cref="CraftingGrid" />'s  <see cref="CraftingGrid.tLastMarkersList" /> yet, 
+	 	it gets initialized to <code>{}</code> in <see cref="CraftingGrid.OnDocumentReady"/>, which will be called
+	 	*after* we have finished, so any setting here is pointless.
+	 	Instead store a reference, so our own OnDocumentReady hook from <see cref="CreateDerivedCraftingGridMetatable" />  
+	 	can copy the value after <see cref="CraftingGrid" />'s call.
+	]]
+	self.tLastMarkersList = setmetatable(tLastMarkers or {}, mtSchematicLog)	
+	self.tColors = tColors or ktHintArrowDefaultColors	
 	
-	setmetatable(tLastMarkers, mtSchematicLog)	
-	
-	-- Do *NOT* deserialize into <see cref="CraftingGrid" />'s  <see cref="CraftingGrid.tLastMarkersList" /> yet, 
-	-- it gets initialized to <code>{}</code> in <see cref="CraftingGrid.OnDocumentReady"/>, which will be called
-	-- *after* we have finished, so any setting here is pointless.
-	-- Instead store a reference, so our own OnDocumentReady hook from <see cref="CreateDerivedCraftingGridMetatable" />  
-	-- can copy the value after <see cref="CraftingGrid" />'s call.
-	self.tLastMarkersList = tLastMarkers
-	
+	if tWindowLocation then
+		self.locSavedWindowLoc = WindowLocation.new(tWindowLocation)	
+	end
+
 	self:CheckForCraftingGridMarkerListInitialization()
 	
 end
+
+
+---------------------------------------------------------------------------------------------------
+-- AthenaConfigForm Functions
+---------------------------------------------------------------------------------------------------
+function Athena:ToggleWindow( wndHandler, wndControl, eMouseButton )
+	if self.wndMain:IsVisible() then
+		self.wndMain:Close()
+	else
+		self:InitializeForm()
+	
+		self.wndMain:Show(true)
+		self.wndMain:ToFront()
+	end
+end
+
+function Athena:WindowMove( wndHandler, wndControl, nOldLeft, nOldTop, nOldRight, nOldBottom )
+	self.locSavedWindowLoc = self.wndMain:GetLocation()
+end
+
+
+function Athena:ColorValueChanged( wndHandler, wndControl, strText )
+	local bFound, _, strKey = strText:find("^(#?%x%x%x%x%x%x%x%x)$")	
+	glog:debug(string.format("ColorValueChanged(%s)", tostring(bFound)))	
+	
+	if bFound then
+		local parent = wndControl:GetParent()
+		local key = parent:GetData()
+
+		self.tColors[key] = strText
+		self:UpdateColorContainer(parent, key)
+	end
+end
+
+function Athena:ColorPickerSignal( wndHandler, wndControl, eMouseButton )
+	local parent = wndControl:GetParent()
+	self.gcolor:ShowColorPicker(self, "OnColorPicked", true, wndControl:GetData(), {container = parent, key= parent:GetData()})
+end
+
+function Athena:OnColorPicked(strColor, token)
+	glog:debug(string.format("OnColorPicked(%s) - key=%s", tostring(strColor), tostring(token and token.key)))
+
+	local key = token.key
+	local container = token.container
+	
+	self.tColors[key] = strColor
+	self:UpdateColorContainer(container, key)
+end
+
 
---
+++ Localization/frFR.lua
@@ -0,0 +1,8 @@
+-----------------------------------------------------------------------------------------------
+-- frFR Localization for Athena
+-- Copyright (c) DoctorVanGogh on Wildstar forums - All Rights reserved
+-----------------------------------------------------------------------------------------------
+
+local L = Apollo.GetPackage("Gemini:Locale-1.0").tPackage:NewLocale("Athena", "frFR")
+if not L then return end
+L["Colors"] = "Couleurs"
